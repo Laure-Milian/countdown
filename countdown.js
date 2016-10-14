047169fa2d995cdabb75ba62483994e78f6577f7
marineco <marinecolonge@gmail.com>
2016-10-14 10:49:48 +0200

    bouton stop/play fonctionnel

--- countdown.js
+++ countdown.js
@@ -1,21 +1,23 @@
 (function main(){
 
+	var stop = false;
+
 	setInterval(function() { 
 
 		var numberSecond = parseInt($('#second').text(),10);
 		var numberMinute = parseInt($('#minute').text(),10);
 		var numberHour = parseInt($('#hour').text(),10);
 
-		if (numberSecond > 0) {
+		if (numberSecond > 0 && stop === false) {
 			numberSecond--;
 			$('#second').text(numberSecond);
 		}
-		else if (numberMinute > 0) {
+		else if (numberMinute > 0 && stop === false) {
 			numberMinute--;
 			$('#minute').text(numberMinute);
 			$('#second').text('60');
 		}
-		else if (numberHour > 0) {
+		else if (numberHour > 0 && stop === false) {
 			numberHour--;
 			$('#hour').text(numberHour);
 			$('#minute').text('60');
@@ -24,22 +26,30 @@
 
 	}, 1000);
 
+	$('#btnStop').on('click', function() {
+		if (stop === false) {
+			stop = true;
+		} else {
+			stop = false;
+		}
+	})
 
-$('#reset').on('click', function(){
+	$('#btnReset').on('click', function(){
 
-	$('#second').text('60');
-	$('#minute').text('60');
-	$('#hour').text('60');
+		$('#second').text('60');
+		$('#minute').text('60');
+		$('#hour').text('60');
 
-});
+	});
 
-$('#btnInput').on('click', function() {
+	$('#btnInput').on('click', function() {
 
-	$('#hour').text($('#inputHour').val());
-	$('#minute').text($('#inputMinute').val());
-	$('#second').text($('#inputSecond').val());
+		$('#hour').text($('#inputHour').val());
+		$('#minute').text($('#inputMinute').val());
+		$('#second').text($('#inputSecond').val());
 
-});
+	});
+
 
 
 })();

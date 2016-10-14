c0b2d4ce87cde88e2ed57cb952cfd453f6ccd0e7
marineco <marinecolonge@gmail.com>
2016-10-14 09:49:58 +0200

    bouton reset fonctionnel

--- countdown.js
+++ countdown.js
@@ -2,12 +2,23 @@
 
 	setInterval(function() { 
 
-		var number = $('h1').text();
-		number--;
-		$('h1').text(number);
+		var number = parseInt($('h1').text(),10);
+		console.log(typeof number);
+
+		if(number > 0) {
+
+			number--;
+			$('h1').text(number);
+		}
 
 	}, 1000);
 
-	
+// changer le text de h1
+
+$('#reset').on('click', function(){
+
+	$('h1').text('10');
+
+});
 
 })();
